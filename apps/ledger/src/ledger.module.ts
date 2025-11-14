a6968806cb1d3cc4f2d56edf542353534138867b
Vadim L. Avramishin <avramishin@gmail.com>
2025-11-14 15:12:37 +0100

    fixes

--- apps/ledger/src/ledger.module.ts
+++ apps/ledger/src/ledger.module.ts
@@ -6,6 +6,7 @@ import { DatabaseModule } from './database/database.module';
 import { LedgerController } from './ledger.controller';
 import { LedgerService } from './ledger.service';
 import { LedgerAccountsService } from './ledger-accounts.service';
+
 import { LedgerAccountsTransactionsService } from './ledger-accounts-transactions.service';
 import configuration from './config/configuration';
 
